366d1baca216ced0cacfec001b36c53317b12f7b
abedarad76 <abedarad76@gmail.com>
2023-12-05 18:53:21 +0330

    searchbox

--- src/page/axios/axios.tsx
+++ src/page/axios/axios.tsx
@@ -11,9 +11,15 @@ export function Axios() {
         last_name: string,
         avatar: string,
     }
+    
+    const [search, setsearch] = useState<string>("");
+
     const [Loding, setLoding] = useState<boolean>(true);
 
     const [cities, setClties] = useState<Citvitrm[]>([]);
+
+    const [datas, setDatas] = useState<Citvitrm[]>([]);
+
 
     async function getlist() {
         try {
@@ -31,16 +37,17 @@ export function Axios() {
 
     async function PostUpdate() {
 
-        let data = {
-            first_name: "AmirMahdi",
-            last_name: "ziaaldini",
-            email: "amirmahdi@gmail.com",
-            avatar: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffa.nody.ir%2F%25D8%25B9%25DA%25A9%25D8%25B3-%25D8%25AF%25D8%25AE%25D8%25AA%25D8%25B1%25D8%25A7%25D9%2586%25D9%2587-%25D8%25A8%25D8%25B1%25D8%25A7%25DB%258C-%25D9%25BE%25D8%25B1%25D9%2588%25D9%2581%25D8%25A7%25DB%258C%25D9%2584%2F&psig=AOvVaw2GydAITdWXTPcaJowrXzIU&ust=1701806689169000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDX_5LK9oIDFQAAAAAdAAAAABAG"
+        const schema = {
+            first_name: "first_name.name",
+            last_name: "product.brand"
+
         }
         try {
-            const post = await axios.post("https://reqres.in/api/users", data);
+            const post = await axios.post<unknown, AxiosResponse<{ data: Citvitrm[] }>>("https://reqres.in/api/users", schema);
             console.log(post);
-            setClties({[...cities,data]})
+            // setDatas();
+          
+
         } catch (erorr) {
             console.log("Api posts ", erorr)
         }
@@ -61,28 +68,32 @@ export function Axios() {
 
     useEffect(() => {
         getlist();
-
     }, [])
     return (
         <div>
             <button onClick={getlist}>onklok</button>
+            <button onClick={() => PostUpdate()}>postUpdate</button>
+            <input type="text" placeholder='seh....' onChange={(e)=>setsearch(e.target.value)} />
+
             {
-                // Loding ? (
-                //     <Lodings />
-                // ) :
-                cities.map((row) => {
+               
+                cities.filter((item)=>{
+                    return search.toLowerCase() === '' ? item : item.first_name.toLowerCase().includes(search)
+                }).map((row) => {
                     return (
                         <div key={row.id}>
                             <img src={row.avatar} alt="" />
-                            <h1>{row.last_name}/name|{row.first_name}</h1>
+                            <h1>{row.last_name}/name|</h1>
+                            <b>{row.first_name}</b>
                             <h2>email | {row.email}</h2>
 
-                            <button onClick={() => PostUpdate()}>postUpdate</button>
+
                             <button onClick={() => putUpdate(row.id)}>Update</button>
                             <button onClick={() => handleDelete(row.id)}>delete</button>
                         </div>
                     )
                 })
             }
+
         </div>)
 }
